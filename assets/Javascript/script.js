82606d7c7fe41c6562f839a137e80c32f22bf346
TharathTho <tharoth.tho@gmail.com>
2020-12-27 20:55:04 +0700

    Snake Game

---
+++ assets/Javascript/script.js
@@ -0,0 +1,119 @@
+const cvs = document.getElementById("snake");
+const ctx = cvs.getContext("2d");
+
+const box = 32;
+
+const ground = new Image();
+ground.src = "../assets/images/ground.png";
+
+const foodImg = new Image();
+foodImg.src = "../assets/images/food.png";
+
+// Snake
+let snake = [];
+snake[0] = {
+    x : 9 * box,
+    y : 10 * box
+};
+// Start Button
+
+
+// Food
+let food = {
+    x : Math.floor(Math.random()*17+1) * box,
+    y : Math.floor(Math.random()*15+3) * box
+}
+
+// Score 
+let score = 0;
+
+//control the snake
+
+let d;
+document.addEventListener("keydown",direction);
+function direction(event){
+    let key = event.keyCode;
+    if( key == 37 && d != "RIGHT"){
+        d = "LEFT";
+    }else if(key == 38 && d != "DOWN"){
+        d = "UP";
+    }else if(key == 39 && d != "LEFT"){
+        d = "RIGHT";
+    }else if(key == 40 && d != "UP"){
+        d = "DOWN";
+    }
+}
+// cheack collision function
+function collision(head,array){
+    for(let i = 0; i < array.length; i++){
+        if(head.x == array[i].x && head.y == array[i].y){
+            return true;
+        }
+    }
+    return false;
+}
+
+// Draw    
+function draw(){
+
+    ctx.drawImage(ground,0,0);
+    
+    for( let i = 0; i < snake.length ; i++){
+        ctx.fillStyle = ( i == 0 )? "green" : "white";
+        ctx.fillRect(snake[i].x,snake[i].y,box,box);
+        
+        ctx.strokeStyle = "red";
+        ctx.strokeRect(snake[i].x,snake[i].y,box,box);
+    }
+    
+    ctx.drawImage(foodImg, food.x, food.y);
+    
+    // old head position
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+    
+    // which direction
+    if( d == "LEFT") snakeX -= box;
+    if( d == "UP") snakeY -= box;
+    if( d == "RIGHT") snakeX += box;
+    if( d == "DOWN") snakeY += box;
+    
+    // if the snake eats the food
+    if(snakeX == food.x && snakeY == food.y){
+        score++;
+        food = {
+            x : Math.floor(Math.random()*17+1) * box,
+            y : Math.floor(Math.random()*15+3) * box
+        }
+        // we don't remove the tail
+    }else{
+        // remove the tail
+        snake.pop();
+    }
+    
+    // add new Head
+    
+    let newHead = {
+        x : snakeX,
+        y : snakeY
+    }
+    
+    // game over
+    var finished = false;
+    if(snakeX < box || snakeX > 17 * box || snakeY < 3*box || snakeY > 17*box || collision(newHead,snake)){
+        clearInterval(game);
+        finished = true;
+        
+    }
+    if(finished==true){
+        ctx.fillText("Game Over!", 200, 220);
+    }
+    
+    snake.unshift(newHead);
+    
+    ctx.fillStyle = "white";
+    ctx.font = "45px Changa one";
+    ctx.fillText(score,2*box,1.6*box);
+}
+
+let game = setInterval(draw,200);
